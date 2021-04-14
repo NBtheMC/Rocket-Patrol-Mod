369e2be98113f36d6799eaeea25197f57be6865a
NBtheMC <nvbhusha@ucsc.edu>
2021-04-13 21:45:05 -0700

    Also added P2

---
+++ src/prefabs/Ship.js
@@ -0,0 +1,22 @@
+class Ship extends Phaser.GameObjects.Sprite{
+    constructor(scene, x, y, texture, frame, pointValue){
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        this.points = pointValue;
+        this.movementSpeed = game.settings.spaceshipSpeed;
+    }
+
+    update(){
+        //move spaceship
+        this.x -= this.movementSpeed;
+        //move from left edge to right edge
+        if(this.x <= 0 - this.width){
+            this.x = game.config.width;
+        }
+    }
+
+    //position reset
+    reset(){
+        this.x = game.config.width;
+    }
+}
